50ac162d0c468860cc5683682f45c367a04b8b2c
rachelez67 <32606826+rachelez67@users.noreply.github.com>
2017-10-24 16:55:05 -0700

    Add files via upload

--- Problem1.java
+++ Problem1.java
@@ -11,22 +11,24 @@ public class Problem1 {
 	public static void wordlist() {
 		Scanner scan = new Scanner(System.in);
 		
-		int wrdlsize = scan.nextInt();	//wrdlsize = wordListSize
-		String wordlist[] = new String[wrdlsize];	
+		int listSize = scan.nextInt();	
+		String wordlist[] = new String[listSize];	
 		
-		for(int c = 0; c < wrdlsize; c++) {	//go thru user input
+		/* Go through user input */
+		for(int c = 0; c < listSize; c++){				
 			wordlist[c] = scan.next();
 		}
 	
-		int numbcount[] = new int[26];	// alphabet size
+		int numbcount[] = new int[26];				 
 		int maxnumber = 0;
 		
-		//word and letter counter
-		for (int i = 0; i < wrdlsize; i++){
+		/* Word and letter counter */
+		for (int i = 0; i < listSize; i++){
 			for(int a = 0; a < wordlist[i].length(); a++){
 				numbcount[wordlist[i].toCharArray()[a]-97]++;
 			}
-		} for(int b = 1; b < 26; b++) {
+		} 
+		for(int b = 1; b < 26; b++) {
 				if (numbcount[maxnumber] < numbcount[b] ) {
 					maxnumber = b;
 				}
